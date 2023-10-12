e2060d09f6967688d242713a68b373cd03afc7a3
Erin Jerri Pangilinan <3184381+erinjerri@users.noreply.github.com>
2023-10-12 14:33:08 -0700

    initial book ex push

--- Chap2_01_HelloWorld/run.py
+++ Chap2_01_HelloWorld/run.py
@@ -4,15 +4,20 @@ load_dotenv()
 import os
 import openai
 
-# Make sure the environment variable OPENAI_API_KEY is set.
+openai.api_key = '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'
 
 # Call the openai ChatCompletion endpoint, with th ChatGPT model
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
-        {"role": "user", "content": "Hello World!"}
-    ]
+      {"role": "user", 
+       "content": "Hello World!"},
+    ],
+temperature=0.7,
+max_tokens=3,
+top_p=1,
+frequency_penalty=0,
+presence_penalty=0,  
 )
-
 # Extract the response
 print(response['choices'][0]['message']['content'])
